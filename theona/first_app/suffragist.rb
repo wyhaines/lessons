21854a9ae343179ade33b4525ba89a92c72fe25f
Theona Haines <theonahaines@gmail.com>
2020-09-21 13:36:06 -0600

    New code in suffragist.rb

--- theona/first_app/suffragist.rb
+++ theona/first_app/suffragist.rb
@@ -9,8 +9,14 @@ get '/' do
  @title = 'Welcome to the Suffragist!'
  erb :index
 end
+
 post '/cast' do
  @title = 'Thanks for casting your vote!'
  @vote = params['vote']
  erb :cast
+end
+
+get '/results' do
+  @votes = { 'HAM' => 7, 'PIZ' => 5, 'CUR' => 3}
+  erb :results
 end
