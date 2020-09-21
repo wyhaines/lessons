d912d07f1eac4ead33d928d0f492ba384e0e5f63
Theona Haines <theonahaines@gmail.com>
2020-09-21 13:06:06 -0600

    new code stuff

--- theona/first_app/suffragist.rb
+++ theona/first_app/suffragist.rb
@@ -6,5 +6,11 @@ Choices = {
 	'NOO' => 'Noodles',
 }
 get '/' do
+ @title = 'Welcome to the Suffragist!'
  erb :index
+end
+post '/cast' do
+ @title = 'Thanks for casting your vote!'
+ @vote = params['vote']
+ erb :cast
 end
