cfd6ef7c2d3fd0ef26ec0128d9140997ae0db595
Theona Haines <theonahaines@gmail.com>
2020-09-22 12:57:22 -0600

    Added code to suffragist

--- theona/first_app/suffragist.rb
+++ theona/first_app/suffragist.rb
@@ -1,10 +1,14 @@
 require 'sinatra'
+
+require 'yaml/store'
+
 Choices = {
 	'HAM' => 'Hamburger',
 	'PIZ' => 'Pizza',
 	'CUR' => 'Curry',
 	'NOO' => 'Noodles',
 }
+
 get '/' do
  @title = 'Welcome to the Suffragist!'
  erb :index
@@ -13,10 +17,18 @@ end
 post '/cast' do
  @title = 'Thanks for casting your vote!'
  @vote = params['vote']
+ @store = YAML::Store.new 'notes.yml'
+ @store.transaction do
+   @store['votes'] ||= {}
+   @store['votes'][@vote] ||= 0
+   @store['votes'][@vote] += 1
+ end
  erb :cast
 end
 
 get '/results' do
-  @votes = { 'HAM' => 7, 'PIZ' => 5, 'CUR' => 3}
-  erb :results
+ @title = 'Results so far:'
+ @store = YAML::Store.new 'votes.yml'
+ @votes = @store.transaction { @store['votes'] }
+ erb :results
 end
